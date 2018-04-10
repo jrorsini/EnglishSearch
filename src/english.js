83ddc4686db521ddebafd376188a6bbc2012be59
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-04-10 16:54:50 +0900

    Added more mocha test. Has preposition function created and unit tested.

--- src/english.js
+++ src/english.js
@@ -1,7 +1,21 @@
-const foo = () => {
-	return 'foo'
+const prepositions = ['off', 'up', 'on', 'from', 'out', 'down', 'away', 'in']
+
+const words_list = sentence => {
+	return sentence
+		.trim()
+		.split(/\s/)
+		.map(e => e.replace(/[\.\,\:\?\!]/gi, ''))
 }
 
+const has_preposition = sentence => {
+	const last_word = words_list(sentence)[words_list(sentence).length - 1]
+	return prepositions.indexOf(last_word) !== -1 ? true : false
+}
+
+const has_phrasal_verb = sentence => {}
+
 module.exports = {
-	foo
+	has_phrasal_verb,
+	has_preposition,
+	words_list
 }
--- test/specs.js
+++ test/specs.js
@@ -2,9 +2,38 @@ var assert = require('assert')
 var english = require('../src/english')
 
 describe('Array', () => {
-	describe('#indexOf()', () => {
-		it('should return -1 when the value is not present', () => {
-			assert.equal(english.foo(), 'foo')
+	describe('#english.words_list()', () => {
+		it("Should break down sentence's word's list", () => {
+			assert.equal(
+				String(english.words_list('drop him off')),
+				String(['drop', 'him', 'off'])
+			)
+		})
+		it('Should handle punctuation such as commas, dots, white spaces etc...', () => {
+			assert.equal(
+				String(english.words_list('drop him off.')),
+				String(['drop', 'him', 'off'])
+			)
+			assert.equal(
+				String(english.words_list(' drop, him off.')),
+				String(['drop', 'him', 'off'])
+			)
+			assert.equal(
+				String(english.words_list('drop him off.')),
+				String(['drop', 'him', 'off'])
+			)
+		})
+	})
+
+	describe('#english.has_preposition()', () => {
+		it("Should break down sentence's word's list", () => {
+			assert.equal(english.has_preposition('drop him off'), true)
+		})
+		it("Should break down sentence's word's list", () => {
+			assert.equal(english.has_preposition('drop him'), false)
+		})
+		it("Should break down sentence's word's list", () => {
+			assert.equal(english.has_preposition('drop him out'), true)
 		})
 	})
 })
