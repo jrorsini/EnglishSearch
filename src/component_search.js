a94b5e6951fdc09a17a32c15841b95a20bd7a2c6
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-04-10 15:13:39 +0900

    Created component file for search bar.

---
+++ src/component_search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/**
+ * @class {JSX} creates Search component.
+ * @return {JSX} input element.
+ */
+export default class Search extends React.Component {
+	render() {
+		return <input type="text" />
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-class Search extends React.Component {
-	render() {
-		return <input type="text" />
-	}
-}
+import Search from './component_search'
 
 ReactDOM.render(<Search />, document.getElementById('app'))
