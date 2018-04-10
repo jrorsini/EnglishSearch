d6b94123fd740ef42200dd41df994c767d090e22
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-04-10 15:08:59 +0900

    Added react and react-dom dependencies Created search Component.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('test')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Search extends React.Component {
+	render() {
+		return <input type="text" />
+	}
+}
+
+ReactDOM.render(<Search />, document.getElementById('app'))
